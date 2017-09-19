aa4cc9c2597234af6be50c3c6c600076d18ca2fd
Sahil <sahilhaque24.10@gmail.com>
2017-09-19 16:55:41 +0530

    Completed header-hero

--- app/assets/scripts/modules/_StickyHeader.js
+++ app/assets/scripts/modules/_StickyHeader.js
@@ -3,7 +3,18 @@ import smoothScroll from 'jquery-smooth-scroll';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class StickyHeader {
-   
+	constructor() {
+   	this.btn4 = $('.btn-4');
+		this.onDocLoad();
+	}
+	
+	onDocLoad() {
+		var that = this;
+		$(document).ready(() => { that.btn4.addClass('btn-4-load')});
+		this.btn4.hover(() => { that.btn4.removeClass('btn-4-load')});
+	}
+		
 }
+
 
 export default StickyHeader;
